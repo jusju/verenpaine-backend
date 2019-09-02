657832dca2b6f94ec508bb12751737090dbcdce2
Juslin Jukka <jusju@haaga-helia.fi>
2019-09-02 13:52:23 +0300

    first commit

---
+++ src/test/java/fi/haagahelia/verenpaine/VerenpaineApplicationTests.java
@@ -0,0 +1,16 @@
+package fi.haagahelia.verenpaine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VerenpaineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
